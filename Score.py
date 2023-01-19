3826c0942135df5ce8bae5d2ce1c13d6dd4bdaf5
Guilherme Diel <guiddiel@gmail.com>
2023-01-19 09:24:11 -0300

    changed the objectives

--- Score.py
+++ Score.py
@@ -10,10 +10,8 @@ def getScore(in_file, out_file):
             continue
         makespan_func = 0
         makespan_reward = 0
-        energy_consuption_func = 0
-        energy_consuption_reward = 0
-        energy_efficiency_func = 0
-        energy_efficiency_reward = 0
+        slowdown_func = 0
+        slowdown_reward = 0
         for index_line, job in jobs.iterrows():
             if job['success'] == 0:
                 continue
@@ -21,10 +19,10 @@ def getScore(in_file, out_file):
             if job['submission_time'] >= current_job['submission_time']:
                 makespan_func += job['finish_time'] - job['submission_time']
 
-            if current_job['starting_time'] <= job['finish_time'] or current_job['finish_time'] >= job['starting_time']:
+            if current_job['starting_time'] <= job['finish_time'] or current_job['finish_time'] >= job['starting_time']: # Active jobs
                 makespan_reward += 0
         
-        jobs_json.append({'Job_id' : current_job['job_id'], 'makespan_function' : makespan_func, 'makespan_reward' : makespan_reward, 'energy_consuption_function' : energy_consuption_func, 'energy_consuption_reward' : energy_consuption_reward, 'energy_efficiency_function' : energy_efficiency_func, 'energy_efficiency_reward': energy_efficiency_reward})
+        jobs_json.append({'Job_id' : current_job['job_id'], 'makespan_function' : makespan_func, 'makespan_reward' : makespan_reward, 'slowdown_function' : slowdown_func, 'slowdown_reward' : slowdown_reward})
 
     with open(out_file, 'w') as out:
         json.dump(jobs_json, out, indent=4)
