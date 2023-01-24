90f01e80abeefdee5840658dbb1c6507e41fae94
Guilherme Diel <guiddiel@gmail.com>
2023-01-24 07:53:29 -0300

    finished

--- Score.py
+++ Score.py
@@ -16,20 +16,20 @@ def getScore(in_file, out_file):
             if job['success'] == 0:
                 continue
 
-            if job['submission_time'] >= current_job['submission_time']:
+            if job['submission_time'] <= current_job['submission_time'] and job['submission_time'] is not None:
                 makespan_func += job['finish_time'] - job['submission_time']
                 slowdown_func += (job['execution_time'] + job['waiting_time'] ) / job['execution_time']
             # slowdown = (running_time + waiting_time) / running_time
 
-            if (job['finish_time'] >= current_job['starting_time'] and job['starting_time'] <= current_job['finish_time']) or (job['starting_time'] <= current_job['starting_time'] and job['finish_time'] >= current_job['finish_time']):
-                allocated_resources = job['allocated_resources'].split('-') # 0 - 15 => [0, 15]
-                makespan_reward += int(allocated_resources[1]) - int(allocated_resources[0])
-                slowdown_reward -= (job['execution_time'] + job['waiting_time'] ) / job['execution_time']
+        allocated_resources = current_job['allocated_resources'].split('-') # 0 - 15 => [0, 15]
+        makespan_reward = int(allocated_resources[1]) - int(allocated_resources[0])
+        slowdown_reward = (current_job['execution_time'] + current_job['waiting_time'] ) / current_job['execution_time']
         
         jobs_json.append({'Job_id' : current_job['job_id'], 'makespan_function' : makespan_func, 'makespan_reward' : makespan_reward, 'slowdown_function' : slowdown_func, 'slowdown_reward' : slowdown_reward})
 
     with open(out_file, 'w') as out:
         json.dump(jobs_json, out, indent=4)
+
 
 
 if __name__ == '__main__':
