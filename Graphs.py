e55145a3a9fbb2217a03991d7a1fcc8608be0566
Guilherme Diel <guiddiel@gmail.com>
2023-01-25 09:11:40 -0300

    removed regression

--- Graphs.py
+++ Graphs.py
@@ -12,15 +12,15 @@ def generate_graphs(in_file, scheduler):
             os.mkdir(path_scheduler)
         except OSError as error:
             print(error)
-    y_axis_regression = []
-    y_axis_CDF = []
-    x_axis_makespan_function = []
-    x_axis_makespan_reward = []
-    x_axis_slowdown_function = []
-    x_axis_slowdown_reward = []
+    y_axis_regression           = []
+    y_axis_CDF                  = []
+    x_axis_makespan_function    = []
+    x_axis_makespan_reward      = []
+    x_axis_slowdown_function    = []
+    x_axis_slowdown_reward      = []
     with open (in_file, 'r') as file:
         data = json.load(file)
-
+    data.sort(key=lambda k : k['starting_time'])
     for d in data:
         y_axis_regression.append(d['starting_time'])
         y_axis_CDF.append(data.index(d) / len(data))
@@ -28,38 +28,9 @@ def generate_graphs(in_file, scheduler):
         x_axis_makespan_reward.append(d['makespan_reward'])
         x_axis_slowdown_function.append(d['slowdown_function'])
         x_axis_slowdown_reward.append(d['slowdown_reward'])
-    
-    # Regression:
-    path_scheduler = 'Graphs/' + scheduler + '/Regression/'
-    if not path.exists(path_scheduler):
-        try:
-            os.mkdir(path_scheduler)
-        except OSError as error:
-            print(error)
-    
-    regression = sns.regplot(x=x_axis_makespan_function, y=y_axis_regression)
-    name = path_scheduler + '_makespan_function.png'
-    plt.savefig(name) 
-    plt.clf()
-
-    regression = sns.regplot(x=x_axis_makespan_reward, y=y_axis_regression)
-    name = path_scheduler + '_makespan_reward.png'
-    plt.savefig(name) 
-    plt.clf()
-
-    regression = sns.regplot(x=x_axis_slowdown_function, y=y_axis_regression)
-    name = path_scheduler + '_slowdown_function.png'
-    plt.savefig(name) 
-    plt.clf()
-
-    regression = sns.regplot(x=x_axis_slowdown_reward, y=y_axis_regression)
-    name = path_scheduler + '_slowdown_reward.png'
-    plt.savefig(name) 
-    plt.clf()
-    
     #CDF
 
-    path_scheduler = 'Graphs/' + scheduler + '/CDF/'
+    path_scheduler = 'Graphs/' + scheduler + '/'
     if not path.exists(path_scheduler):
         try:
             os.mkdir(path_scheduler)
