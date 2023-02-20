4774f9bf0b273faecbca4966d69629e9c1d096f1
Guilherme Koslovski <gkoslovski@bagual-2.local>
2023-02-20 16:04:27 -0300

    scores and graphs

--- Graphs.py
+++ Graphs.py
@@ -1,88 +1,57 @@
 import matplotlib.pyplot as plt
-import argparse as ap
 import json
 import os
 from os import path
+import sys
 
 def generate_graphs(in_file):
-    tuple_makespan              = []
+    tuple_revenue               = []
     tuple_slowdown              = []
     path_scheduler = 'Graphs/'
 
     for file in in_file:
         y_axis                  = []
         x_axis                  = []
-        name = file.split('/')[len(file.split('/')) - 2] # name.txt => name
-
+        name = file.split('/')[len(file.split('/')) - 1].replace(".json", "") # name.txt => name
+        print('Processing "{}"...'.format(name))
         try:
             with open (file, 'r') as f:
                 data = json.load(f)
         except FileNotFoundError:
             print('File "{}" not found'.format(file))
-        
+        i = 0 
         for d in data:
-            y_axis.append(data.index(d))
-            x_axis.append(d['makespan'])
-        
-        plt.ylabel("Cumulative Distribution Function") 
-            # t[0].sort()
-        plt.scatter(x_axis, y_axis)
-        name = path_scheduler + '_makespan_no_sort.png'
-        plt.xlabel("Makespan Function")
-        plt.legend(loc='best')
-        plt.savefig(name) 
-        plt.clf()
-
-        for d in data:
-            y_axis.append(data.index(d))
-            x_axis.append(d['slowdown'])
-        
-        plt.ylabel("Cumulative Distribution Function") 
-            # t[0].sort()
-        plt.scatter(x_axis, y_axis)
-        name = path_scheduler + '_slowdown_no_sort.png'
-        plt.xlabel("Slowdown Function")
-        plt.legend(loc='best')
-        plt.savefig(name) 
-        plt.clf()
-
-        data.sort(key=lambda k : k['makespan'])
-        for d in data:
-            y_axis.append(data.index(d) / len(data))
-            x_axis.append(d['makespan'])
-        tuple_makespan.append((x_axis, name, y_axis))
+            x_axis.append(i)
+            y_axis.append(d['revenue'])
+            i+=1
+        tuple_revenue.append((name, x_axis, y_axis))        
+        print('Revenue done...')
 
         y_axis = []
         x_axis = []
         data.sort(key=lambda k : k['slowdown'])
+        i = 0
         for d in data:
-            x_axis.append(d['slowdown'])
-            y_axis.append(data.index(d) / len(data))
-        tuple_slowdown.append((x_axis, name,y_axis)) 
-        
-    #CDF
-    if not path.exists(path_scheduler):
-        try:
-            os.mkdir(path_scheduler)
-        except OSError as error:
-            print(error)
+            x_axis.append(float(d['slowdown']))
+            y_axis.append(float(i / len(data)))
+            i+=1
+        tuple_slowdown.append((name, x_axis, y_axis)) 
+        print('Slowdown done...')
 
-    plt.ylabel("Cumulative Distribution Function") 
-    for t in tuple_makespan:
-        # t[0].sort()
-        plt.plot(t[0], t[2], label=t[1])
-    name = path_scheduler + '_makespan.png'
-    plt.xlabel("Makespan")
+    #revenue
+    plt.ylabel("Revenue") 
+    for t in tuple_revenue:
+        plt.plot(t[1], t[2], label=t[0])
+    name = path_scheduler + 'revenue.png'
+    plt.xlabel("Time (seconds)")
     plt.legend(loc='best')
     plt.savefig(name) 
     plt.clf()
 
-    # tuple_slowdown.sort()
-    plt.ylabel("Cumulative Distribution Function") 
+    plt.ylabel("CDF") 
     for t in tuple_slowdown:
-        # t[0].sort()
-        plt.plot(t[0], t[2], label=t[1])
-    name = path_scheduler + '_slowdown.png'
+        plt.plot(t[1], t[2], label=t[0])
+    name = path_scheduler + 'slowdown.png'
     plt.xlabel("Slowdown")
     plt.legend(loc='best')
     plt.savefig(name) 
@@ -90,8 +59,4 @@ def generate_graphs(in_file):
 
 
 if __name__ == '__main__':
-    parse = ap.ArgumentParser()
-    parse.add_argument('-f','--files_list', action='append', help='In FIles (json)', required=True)
-    arg = parse.parse_args()
-
-generate_graphs(arg.files_list)
+    generate_graphs(sys.argv[1:])
--- Score.py
+++ Score.py
@@ -6,24 +6,23 @@ def getScore(in_file, out_file):
     jobs = list(pd.read_csv(in_file).sort_values(by='submission_time').iterrows())
     jobs_json = []
     for index_current, current_job in jobs:
-        print(index_current)
         if current_job['success'] == 0:
             continue
-        makespan = 0
+        revenue = 0
         slowdown = 0
         starting_time = current_job['starting_time']
-        # slowdown = (running_time + waiting_time) / running_time
-
         multi_allocated_resources = current_job['allocated_resources'].split() # 0-15 16-17 => [0-15, 16-17]
         for x in multi_allocated_resources:
             allocated_resources = x.split('-')
             try:
-                makespan = 1 + int(allocated_resources[1]) - int(allocated_resources[0])
+                revenue += 1 + int(allocated_resources[1]) - int(allocated_resources[0])
             except IndexError:
-                makespan = int(allocated_resources[0]) + 1 if int(allocated_resources[0]) != 0 else 1
-        slowdown = (current_job['execution_time'] + current_job['waiting_time'] ) / current_job['execution_time']
-        
-        jobs_json.append({'Job_id' : current_job['job_id'], 'makespan' : makespan, 'slowdown' : slowdown, 'starting_time' : starting_time})
+                revenue += int(allocated_resources[0]) + 1 if int(allocated_resources[0]) != 0 else 1
+            slowdown = max(current_job['turnaround_time'] / max(current_job['execution_time'], 10), 1)
+            if slowdown > 1000.0:
+                print(current_job)
+
+        jobs_json.append({'Job_id' : current_job['job_id'], 'revenue' : revenue, 'slowdown' : slowdown, 'starting_time' : starting_time})
 
     with open(out_file, 'w') as out:
         json.dump(jobs_json, out, indent=4)
